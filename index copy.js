7146ad39d2de4a81ed385419fb382b22b08151f9
Samy Fadel <samyfadel@google.com>
2022-12-16 16:13:08 +0100

    samy

--- index copy.js
+++ index copy.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 80;
 const Firestore = require('@google-cloud/firestore');
-var cors = require('cors')
-app.options('*', cors())
-app.use(require('body-parser').json());
+//var cors = require('cors');
+//app.options('*', cors());
+app.use(require('body-parser').json()); //ok
 
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
@@ -13,7 +13,9 @@ initializeApp({
   credential: applicationDefault()
 });
 
-var whitelist = ['http://34.160.109.71:80', 'http://34.160.109.71:3000', 'http://34.160.109.71/microservice2', 'http://34.160.109.71/microservice1']
+const db = getFirestore();
+
+/* var whitelist = ['http://34.160.109.71:80', 'http://34.160.109.71:3000', 'http://34.160.109.71/microservice2', 'http://34.160.109.71/microservice1', 'http://34.160.51.212:80', 'http://34.160.51.212:80/readiness', 'http://10.63.0.141:80/readiness', 'http://10.63.0.141:80']
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -22,18 +24,21 @@ var corsOptions = {
       callback(new Error('Not allowed by CORS'))
     }
   }
-}
+} */
 
 /* const db = new Firestore({
   projectId: 'gkemedium',
   keyFilename: '/Users/samyfadel/Downloads/gkemedium-1d285a7a4755.json',
 }); */
 
-app.get('/readiness', cors(corsOptions), (req, res) => {
+
+
+app.get('/readiness', (req, res) => {
     res.send('<h1>pod container is ready!</h1>');
  });
 
-app.post('/microservice2', cors(corsOptions), (req,res) => {
+
+ app.post('/microservice2', (req,res) => {
 
   console.log(req.body);
 
@@ -45,6 +50,7 @@ app.post('/microservice2', cors(corsOptions), (req,res) => {
     postDoc().then(res.send("bravo"))
 
 });
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 80;
-const Firestore = require('@google-cloud/firestore');
-//var cors = require('cors');
-//app.options('*', cors());
-app.use(require('body-parser').json()); //ok
+var cors = require('cors');
+app.use(cors());
+app.options('*', cors());
+app.use(require('body-parser').json());
 
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
@@ -13,24 +13,8 @@ initializeApp({
   credential: applicationDefault()
 });
 
+
 const db = getFirestore();
-
-/* var whitelist = ['http://34.160.109.71:80', 'http://34.160.109.71:3000', 'http://34.160.109.71/microservice2', 'http://34.160.109.71/microservice1', 'http://34.160.51.212:80', 'http://34.160.51.212:80/readiness', 'http://10.63.0.141:80/readiness', 'http://10.63.0.141:80']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-} */
-
-/* const db = new Firestore({
-  projectId: 'gkemedium',
-  keyFilename: '/Users/samyfadel/Downloads/gkemedium-1d285a7a4755.json',
-}); */
-
 
 
 app.get('/readiness', (req, res) => {
